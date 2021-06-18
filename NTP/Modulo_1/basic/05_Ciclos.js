88b9d9953f7db15ca31ed14e3b568ee84f6d60c2
Anderson Perez <pand9390@gmail.com>
2021-06-18 12:07:35 -0500

    IntroduccionJS

---
+++ NTP/Modulo_1/basic/05_Ciclos.js
@@ -0,0 +1,22 @@
+// While
+
+while (condition) {
+    
+}
+
+// For 
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+// for in
+ var persona = {
+     nombre: "Anderson",
+     apellido: "Perez",
+     viajes:["",""]
+     }
+
+    for (const key in persona) {
+       console.log(persona[key])
+    }
