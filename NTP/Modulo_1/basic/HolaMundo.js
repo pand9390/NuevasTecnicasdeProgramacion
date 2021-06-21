c96bcd396cb03d996015bdb742e57e7610ad3232
Anderson Perez <pand9390@gmail.com>
2021-06-20 19:25:55 -0500

    Deber-Console

--- NTP/Modulo_1/basic/HolaMundo.js
+++ NTP/Modulo_1/basic/HolaMundo.js
@@ -1,1 +1,24 @@
 console.log("Hola Mundo");
+
+// Console.assert()
+// Registra un mensaje y envía una traza de error a la consola si el primer argumento es false
+
+// Console.dir()
+// Muestra un listado interactivo de las propiedades de un objeto JavaScript específico.
+// Esta lista le permite examinar el contenido de los objetos hijos.
+
+// Console.error()
+// Muestra un mensaje de error. Se pueden utilizar sustituciones de cadena y argumentos adicionales con este método.
+
+// Console.info()
+// Muestra un mensaje de información en la Consola Web. 
+// Se puede utilizar sustitución de cadena y argumentos adicionales con este método.
+
+// Console.log()
+// Para mensajes generales de registro de información. 
+// Se puede utilizar sustitución de cadena y argumentos adicionales con este método.
+
+// Console.time()
+// Inicia un temporizador con un nombre específico como parámetro.
+// Hasta 10.000 temporizadores simultáneos pueden ejecutarse en una página determinada.
+
