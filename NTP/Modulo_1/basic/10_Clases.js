b8fc963f9163fc08f01e3616c14a6c700361390f
Anderson Perez <pand9390@gmail.com>
2021-06-23 19:33:34 -0500

    deber-clases

--- NTP/Modulo_1/basic/10_Clases.js
+++ NTP/Modulo_1/basic/10_Clases.js
@@ -25,7 +25,7 @@ class Rectangulo2{
     }
 }
 var rectangulo2 = new Rectangulo2(6, 3)
-console.log(rectangulo2.calcularArea());
+// console.log(rectangulo2.calcularArea());
 
 /* DEBER 
     Clase persona
@@ -33,3 +33,48 @@ console.log(rectangulo2.calcularArea());
     -implementar metodos estaticos - Punto, distancia
     -Construir clases Persona y estudiante, extender (extends) Estudiante con Persona
 */
+class Persona{
+    constructor(nombre, edad){
+        this.nombre = nombre
+        this.edad = edad
+    }
+    saludar(){
+        console.log(`hola mi nombre es ${this.nombre} y tengo ${this.edad} años`)
+    }
+    static punto(){
+        console.log(`este es un metodo estatico`)
+    }
+}
+// var persona = new Persona('Anderson', 24);
+// persona.saludar()
+// Persona.punto()
+
+var Persona2 = {
+    nombre:'Anderson',
+    apellido:'Pérez',
+    getNombre: function() {
+        return this.nombre;
+    },
+
+    setNombre: function(nombre) {
+        this.nombre = nombre;
+    },
+}
+Persona2.setNombre("Paul");
+console.log(Persona2.getNombre());
+
+
+
+class Estudiante extends Persona{
+    constructor(nombre,edad,carrera,semestre){
+        super(nombre,edad);
+        this.carrera = carrera
+        this.semestre = semestre
+    }
+    saludoyestudia(){
+        this.saludar();
+        console.log(`Estudio la carrera de ${this.carrera} y estoy en ${this.semestre} semestre`)
+    }
+}
+var estudiante = new Estudiante('Anderson', 24, 'Analisis de sitemas',6);
+console.log(estudiante.saludoyestudia());
