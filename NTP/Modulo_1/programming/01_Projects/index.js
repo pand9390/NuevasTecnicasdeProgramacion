b9853c6f59b805d946ecfd88ca5b3b2efd462ab5
Anderson Perez <pand9390@gmail.com>
2021-06-25 11:42:32 -0500

    router.get

--- NTP/Modulo_1/programming/01_Projects/index.js
+++ NTP/Modulo_1/programming/01_Projects/index.js
@@ -2,31 +2,12 @@ const express = require('express');
 
 //genera una aplicacion express
 const app = express()
-//EndPoint
-app.get('/',(req, res)=>{
-    res.send("Hola mundo con expressJs y nodemon")
-})
+//Routes
+const{ RouterIndex } = require('./routes/index');
 
-app.get("/saludo",(req, res)=>{
-    //req.query
-    // console.log(req.query);
-
-    //<<<<desestructuracion
-    const {query: {nombre, apellido}}= req;
-    res.json({
-        saludo:`Hola soy ${nombre} ${apellido}`,
-    })
-});
-    //>>>>>>>>>>>>>>>>>>>>>>>
-
-app.get('/saludo/:nombre/:apellido', (req, res) => {
-    const {params} = req;
-    res.json({
-        nombre: params.nombre,
-        apellido: params.apellido
-
-    })
-})
+app.use("/", RouterIndex)
+// app.use("/user", RouterIndex)
+// app.use("/user/saludo", RouterIndex)
 
 //Levanto el servidor
 
--- NTP/Modulo_1/programming/01_Projects/routes/index.js
+++ NTP/Modulo_1/programming/01_Projects/routes/index.js
@@ -1,12 +1,38 @@
 // traemos el paquete
 const {Router} = require('express');
-
+// const data = require("./data");
 //Inicializamos Router
 const router = Router()
 
 //Creacion de endpoints
 
+router
+.get("/", (req, res)=>{
+    res.send("Hola mundo con expressJs y nodemon")
+})
+.get("/saludo",(req, res)=>{
+    const {query: {nombre, apellido}}= req;
+    res.json({
+        saludo:`Hola soy ${nombre} ${apellido}`,
+    })
+})
+.get("/saludo/:nombre/:apellido", (req,res)=>{
+    const {params} = req;
+    res.json({
+        nombre: params.nombre,
+        apellido: params.apellido
+})
+})
+// .get("/users", (req, res)=>{
+//     res.send({
+//         msg:"lista de usuarios",
+//         body: data
+//     })
+// })
 
+
+
+// exportamos las rutas 
 
 module.exports.RouterIndex = router     //nos permite exportar el archivo
 
